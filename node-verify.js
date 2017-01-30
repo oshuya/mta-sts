35122f7f942e086041cb4f0392140a5ca4b40a94
shuya <shuya@sfc.wide.ad.jp>
2017-01-31 02:59:46 +0900

    set commandline argument

--- node-verify.js
+++ node-verify.js
@@ -1,22 +1,29 @@
+'use strict'
+
 let https = require('https');
-let url = 'https://mta-sts.yahoo.com/.well-known/mta-sts.json';
+let domain = process.argv[2];
+let url = 'https://mta-sts.'+ domain +'/.well-known/mta-sts.json';
 
-https.get(url, (res) => {
-    let body = '';
-    res.setEncoding('utf-8');
+function verify(){
+    let responce = https.get(url, (res) => {
+        //おまじない
+        let body = '';
+        res.setEncoding('utf-8');
+        res.on('data', (chunk) => {
+            body += chunk;
+        });
 
-    res.on('data', (chunk) => {
-        body += chunk;
+        res.on('end', (res) => {
+            res = JSON.parse(body);
+            let mtaSTS = res;
+            //console.log(mtaSTS);
+            if (mtaSTS.version === 'STSv1') {
+                console.log("Valid Policy");
+            }
+        //console.log("work")
+        });
+    }).on('error', (e) => {
+        console.log(e.message);
     });
-
-    res.on('end', (res) => {
-        res = JSON.parse(body);
-        let mtaSTS = res;
-        //console.log(mtaSTS.version);
-        if (mtaSTS.version === 'STSv1') {
-            console.log("it works");
-        }
-    });
-}).on('error', (e) => {
-    console.log(e.message);
-});
+};
+verify(url);
