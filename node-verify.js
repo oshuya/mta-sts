da9e3e323d906516c11c9c5f1e0b2019cb4ad2c1
shuya <shuya@sfc.wide.ad.jp>
2017-02-01 03:02:53 +0900

    make funnction

--- node-verify.js
+++ node-verify.js
@@ -4,25 +4,26 @@ let https = require('https');
 let domain = process.argv[2];
 const URL = 'https://mta-sts.'+ domain +'/.well-known/mta-sts.json';
 
-function verify(){
-    let responce = https.get(URL, (res) => {
-        //おまじない
-        let body = '';
-        res.setEncoding('utf-8');
-        res.on('data', (chunk) => {
-            body += chunk;
-        });
+function version (mtaSTS) {
+    if (mtaSTS.version === 'STSv1'){
+        console.log("Valid Policy")
+        return true;
+    }
+}
 
-    let webpki = res.on('end', (res) => {
-            res = JSON.parse(body);
-            let mtaSTS = res;
-            if (mtaSTS.version === 'STSv1') {
-                console.log("Valid Policy");
-            }
-            return console.log("true");
-        });
-    }).on('error', (e) => {
-        console.log(e.message);
+let responce = https.get(URL, (res) => {
+    //おまじない
+    let body = '';
+    res.setEncoding('utf-8');
+    res.on('data', (chunk) => {
+        body += chunk;
     });
-};
-verify();
+
+let webpki = res.on('end', (res) => {
+        res = JSON.parse(body);
+        let mtaSTS = res;
+        version(mtaSTS);
+    });
+}).on('error', (e) => {
+    console.log(e.message);
+});
